4052db8799856e4687421a5d4dcd6c6aa15463f7
Kyle C <kyclutter@gmail.com>
2019-06-15 09:46:20 -0500

    updates

---
+++ src/Components/Homepage.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+
+
+class HomePage extends Component {
+
+
+    render(){
+
+
+        return(
+            <div className="homepage-container">
+
+            </div>
+        )
+    }
+}
+
+
+export default HomePage;
