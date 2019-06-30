101bf668255805dafe03ca05510fa56d5160316c
Kyle C <kyclutter@gmail.com>
2019-06-30 17:02:34 -0500

    updates

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import MapContainer from'./map';
+import MapContainer from "./map";
 
 class Pricing extends Component {
   render() {
@@ -10,7 +10,31 @@ class Pricing extends Component {
           <div className="pricing__header-border" />
         </div>
         <MapContainer />
-
+        <div className="pricing__info">
+          <div className="pricing__info-1" />
+          <div className="pricing__info-1-text">
+            <div className="pricing__info-text">Same-Day: $185.00</div>
+            <div className="pricing__info-text">Rush: $125.00</div>
+            <div className="pricing__info-text">Routine: $85.00</div>
+          </div>
+          <div className="pricing__info-2" />
+          <div className="pricing__info-2-text">
+            <div className="pricing__info-text">Same-Day: $145.00</div>
+            <div className="pricing__info-text">Rush: $100.00</div>
+            <div className="pricing__info-text">Routine: $75.00</div>
+          </div>
+          <div className="pricing__info-3" />
+          <div className="pricing__info-3-text">
+            <div className="pricing__info-text">Same-Day: $125.00</div>
+            <div className="pricing__info-text">Rush: $85.00</div>
+            <div className="pricing__info-text">Routine: $65.00</div>
+          </div>
+          <div className="pricing__def">
+            <div className="pricing__def-text">Same-Day: Attempts are made the same day the documents are received </div>
+            <div className="pricing__def-text">Rush: Attempts are made no less than 48 hours of receiving the documents </div>
+            <div className="pricing__def-text">Routine: Attempts are made 3-4 days of receiving the documents </div>
+          </div>
+        </div>
       </div>
     );
   }
